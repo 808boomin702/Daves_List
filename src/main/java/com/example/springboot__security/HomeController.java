4ad265cc19b14bfe359d71595c835c426099b64c
808bgit config --global user.name yourUsername <yourEmail@domain.com>
2021-08-05 00:22:07 -0700

    Initial commit

--- src/main/java/com/example/springboot__security/HomeController.java
+++ src/main/java/com/example/springboot__security/HomeController.java
@@ -1,11 +1,24 @@
 package com.example.springboot__security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.security.Principal;
 
 @Controller
 public class HomeController {
 
+    @Autowired
+    UserRepository userRepository;
+
+    @RequestMapping("/secure")
+    public String secure(Principal principal, Model model){
+        String username = principal.getName();
+        model.addAttribute("user",userRepository.findByUsername(username));
+        return "secure";
+    }
     @RequestMapping("/")
     public String index() {
         return "index2";
@@ -18,7 +31,7 @@ public class HomeController {
 
     @RequestMapping("/admin")
     public String admin() {
-        return "index2";
+        return "admin";
     }
     @RequestMapping("/logout")
     public String logout(){
